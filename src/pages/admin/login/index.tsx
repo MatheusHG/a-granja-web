4d33c2fb060e4f2b48775e672b0480e9885d24c0
Matheus Henrique <matheus.oliveira@ccc.ufcg.edu.br>
2020-07-08 01:07:26 -0300

    success: page login admin

--- src/pages/admin/login/index.tsx
+++ src/pages/admin/login/index.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import { FiUser, FiUnlock, FiLogIn, FiArrowRight } from 'react-icons/fi'
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
-import logo from '../../assets/logo.svg';
+import logo from '../../../assets/logo.png';
 
 const loginAdmin = () => {
   return (
     <div id="page-home">
       <div className="content">
-        <img src={logo} alt="Granja" />
+
+        <img src={logo} alt="Granja" width="180vw" height="180vh" style={{margin: '10px'}} />
+
+        <div className="form-login-user">
+          <FiUser size='18px' />
+          <input type="text" name="text" placeholder="User" />
+        </div>
+        <div className="form-login-lock">
+          <FiUnlock size='18px' />
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+
+        <Link to="/create-point">
+          <strong>Entrar</strong>
+          <span>
+            <FiArrowRight size='20px' color='white' />
+          </span>
+        </Link>
       </div>
     </div>
   );

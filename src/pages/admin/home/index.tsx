2d4599819b7d40f0e9701f6e03a4c9f48aa62629
Matheus Henrique <matheus.oliveira@ccc.ufcg.edu.br>
2020-07-08 16:22:02 -0300

    not completed

--- src/pages/admin/home/index.tsx
+++ src/pages/admin/home/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import {FiLogIn} from 'react-icons/fi';
 
 import './styles.css';
 
+import reset from './../../../assets/reset.svg';
+
 const homeAdmin = () => {
   return (
-    <div>
-      <h1>Login do Admin</h1>
+    <div className="page-home">
+      <div className="header">
+        <h1>Hello World!</h1>
+        <button>
+          <FiLogIn size='80px' color='white' />
+        </button>
+      </div>
+      <div className="box">
+        <div className="box-header">
+          <button>
+            <img src={reset} alt="reset" width="50vh" height="50vh" />
+          </button>
+          <h1>PARTICIPANTES</h1>
+        </div>
+      </div>
     </div>
   );
 };

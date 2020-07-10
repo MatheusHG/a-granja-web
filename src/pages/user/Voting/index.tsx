66369acb16b2341f90f4dbb63d9b0e2aabbd8122
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-07-10 00:43:47 -0300

    recaptcha verification added

--- src/pages/user/Voting/index.tsx
+++ src/pages/user/Voting/index.tsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 import './styles.css';
 import './response.css';
@@ -9,6 +10,7 @@ import User from '../../../components/User';
 import ClosedVoting from './components/closed';
 
 function Voting() {
+  const [captcha, setCaptcha] = useState<string | null>('');
   const [isOut, setIsOut] = useState<boolean>(false);
   const [users, setUsers] = useState<UserInterface[]>([]);
   const [
@@ -31,10 +33,14 @@ function Voting() {
   };
 
   const handleVote = async () => {
-    if (selected) {
-      await api.put('/users', {id: selected?._id});
+    if (selected && captcha) {
+      await api.put('/users', {id: selected?._id, clientResponse: captcha});
     }
   };
+
+  const handleCaptcha = (value: string | null) => setCaptcha(value);
+
+  const reCaptchaSecret = process.env.REACT_APP_SECRET || '';
 
   return (
     !isOut ?
@@ -58,6 +64,11 @@ function Voting() {
               ))
             }
           </div>
+
+          <ReCAPTCHA
+            sitekey={reCaptchaSecret}
+            onChange={handleCaptcha}
+          />
 
           <button type="submit" className="voting-button" onClick={handleVote}>
           Votar

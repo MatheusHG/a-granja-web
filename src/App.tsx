34d2d162ddcfef458cfeb58a02e3781128457f25
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-07-12 20:37:35 -0300

    noopener added on rel link

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ function App() {
       <Routes />
       <footer className="home-footer">
         <h3>
-          Desenvolvido por <a target="_blank" rel="noreferrer" href="https://www.instagram.com/matheushgo_/">Matheus Henrique </a>
-          e <a target="_blank" rel="noreferrer" href="https://www.instagram.com/davig_sousa/">Davi Gomes</a>
+          Desenvolvido por <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/matheushgo_/">Matheus Henrique </a>
+          e <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/davig_sousa/">Davi Gomes</a>
         </h3>
       </footer>
     </>
